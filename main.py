d44e14289a66b6be7f5a6f030e41b8e18b2653e0
fa-riz <muhammed.fariz@erstechnology.com>
2026-08-10 17:50:37 +0530

    10th aug learnign

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
-print("hello world")
+from fastapi import FastAPI, HTTPException, Response, status
+from fastapi.params import Body
+from pydantic import BaseModel
+
+class Post(BaseModel):
+    id: int | None = None
+    title: str
+    content: str
+
+
+app = FastAPI()
+
+@app.get("/")                                    # decoratr - '@'
+async def root():                                #plain function that returns a JSON response with a message "Hello World"  
+    return {"message": "Hello World ffdfd!!!!!!"}
+
+data = {"posts": [
+        {"id": 1, "title": "First Post", "content": "This is the first post."},
+        {"id": 2, "title": "Second Post", "content": "This is the second post."},
+        {"id": 3, "title": "Third Post", "content": "This is the third post."}
+    ]}
+
+@app.get("/posts")                                    # decoratr - '@'
+async def get_posts():
+    return data
+
+@app.post("/posts")                                    # decoratr - '@'
+async def create_post(payload: Post):
+    new_post = payload.dict()
+    new_post["id"] = payload.id if payload.id else len(data["posts"]) + 1
+    data["posts"].append(new_post)
+    return new_post
+    
+    
+
+@app.get("/posts/latest")
+def get_latest_post():
+    if data["posts"]:
+        latest_post = data["posts"][-1]
+        return latest_post
+    return {"error": "No posts available"}
+
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int,Response: Response):
+    for post in data["posts"]:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
+
+@app.delete("/posts/{post_name}")
+def delete_post(post_name: str):
+    for post in data["posts"]:
+        if post["title"] == post_name:
+            del_post = post
+            data["posts"].remove(post)
+            return {"message": f"Post with title '{post_name}' deleted successfully with id {del_post['id']}."}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with title '{post_name}' not found")
